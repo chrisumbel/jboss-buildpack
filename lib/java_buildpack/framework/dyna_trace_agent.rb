415f849680371974e72915682191d18109147286
tom-collings <tcollings@MacBook-Pro.local>
2015-09-29 13:29:18 -0600

    adding agent name support

--- lib/java_buildpack/framework/dyna_trace_agent.rb
+++ lib/java_buildpack/framework/dyna_trace_agent.rb
@@ -59,7 +59,7 @@ module JavaBuildpack
       end
 
       def agent_name
-        "#{@application.details['application_name']}_#{profile_name}"
+        @environment['dynatrace_agent_name'] ? "@environment['dynatrace_agent_name'] : "#{@application.details['application_name']}_#{profile_name}"
       end
 
       def architecture
